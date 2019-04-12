a7b0cab474f0e16aa0d11bd05356b7362eaa3e37
hiyongbyun <35124540+hiyongbyun@users.noreply.github.com>
2019-04-12 02:18:06 -0600

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -21,10 +21,9 @@ train_data <- read.table("UCI HAR Dataset/train/X_train.txt", col.names = featur
 merged_data <- rbind(test_data, train_data)
 
 ##Merge subject ID, activity labels and feature vector data
-merged_data <- cbind(activity, merged_data)
-merged_data <- cbind(subject, merged_data)
+merged_data <- cbind(subject, activity, merged_data)
 
-##Select variables containing activity, mean, or std
+##Select variables containing subject, activity, mean, and std
 mean_std_data <- select(merged_data, subject, activity, matches("mean|std"))
 
 ##Name activities
